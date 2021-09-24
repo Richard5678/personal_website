21d81fdcd940d53b32204d8d5a0fd1012f9ab0ec
Richard5678 <frqrichard@gmail.com>
2021-09-24 15:28:41 +0800

    deleted whitenoice

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiceMiddleware',
+    #'whitenoise.middleware.WhiteNoiceMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoice.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoice.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
